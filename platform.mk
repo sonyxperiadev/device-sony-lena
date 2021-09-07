8742975f54179ddc7f1142f1f77d6967e459c57d
Alin Jerpelea <alin.jerpelea@sony.com>
2021-09-07 09:30:53 +0200

    platform: add audiopd service
    the audiopd service is moved to platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -77,7 +77,6 @@ AUDIO_FEATURE_ENABLED_USB_BURST_MODE := true
 AUDIO_FEATURE_SONY_CIRRUS := true
 
 # DSP
-TARGET_NEEDS_AUDIOPD := true
 TARGET_NEEDS_SDSP_SENSORS_PDR := true
 
 # Dynamic Partitions: Enable DP
@@ -225,6 +224,10 @@ PRODUCT_PACKAGES += \
     init.lena \
     init.lena.pwr \
     ueventd
+
+# Audio init
+PRODUCT_PACKAGES += \
+    audiopd.rc \
 
 # Audio
 PRODUCT_PACKAGES += \
